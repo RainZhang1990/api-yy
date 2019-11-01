53de59ce4a069378db5682fcf4112faa0d419290
Rain Zhang <rainzhang1990@aliyun.com>
2019-11-01 15:15:02 +0800

    ...

--- start.py
+++ start.py
@@ -19,7 +19,7 @@ from handler import app,order_batch,spu_classify
 def make_app():
     settings = {
         "debug": options.debug,
-        "app_name": "API-GATEWAY",
+        "app_name": "API-YY",
         "compress_response": True,
         "template_path" : 'templates', #模板文件目录,想要Tornado能够正确的找到html文件，需要在 Application 中指定文件的位置
         "static_path" : 'static'  #静态文件目录,可用于用于访问js,css,图片之类的添加此配置之后，tornado就能自己找到静态文件
