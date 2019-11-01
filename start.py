03c5585c440f40f89351edac4bb67a341645df22
Rain Zhang <rainzhang1990@aliyun.com>
2019-11-01 15:53:21 +0800

    s

--- start.py
+++ start.py
@@ -19,7 +19,7 @@ from handler import app,order_batch,spu_classify
 def make_app():
     settings = {
         "debug": options.debug,
-        "app_name": "API-YY",
+        "app_name": Config().name,
         "compress_response": True,
         "template_path" : 'templates', #模板文件目录,想要Tornado能够正确的找到html文件，需要在 Application 中指定文件的位置
         "static_path" : 'static'  #静态文件目录,可用于用于访问js,css,图片之类的添加此配置之后，tornado就能自己找到静态文件
@@ -60,7 +60,7 @@ def main():
     app = make_app()
     server = tornado.httpserver.HTTPServer(app, xheaders=True)
     server.bind(options.port)
-    server.start(4)
+    server.start(options.thread)
     # server.listen(options.port)
     logging.info("{} Server started on port: {}".format(Config().name, options.port))
     IOLoop.current().start()
@@ -76,6 +76,7 @@ def init_config():
 
     # set options
     options.define("debug", default=Config().debug)
+    options.define("thread", default=Config().thread)
     options.define('port', default=Config().port,
                    help='run on the given port', type=int)
 
