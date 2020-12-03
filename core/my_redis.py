d18aec1fa69345a93f8e8e37926573f244ac43c9
Rain Zhang <rainzhang1990@aliyun.com>
2020-12-03 15:37:54 +0800

    'redis'

--- core/my_redis.py
+++ core/my_redis.py
@@ -22,7 +22,7 @@ def listen():
     _thread.start_new_thread(msg_process, ())
 
 def msg_process():
-    logging.info('redis msg listening, pid:{} ppid:{}'.format(msg, os.getpid(), os.getppid()))
+    logging.info('redis msg listening, pid:{} ppid:{}'.format(os.getpid(), os.getppid()))
     retry_interval = Config().redis.get('retry_interval')
     while True:
         try:
